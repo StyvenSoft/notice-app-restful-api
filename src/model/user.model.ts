43ada95668bd605cd54188efc3fb10d3ee516185
Steveen <steveen.echeverri@gmail.com>
2021-08-16 17:22:34 -0500

    Created model User schema for loggin

---
+++ src/model/user.model.ts
@@ -0,0 +1,34 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+
+export interface UserDocument extends mongoose.Document {
+    email: string;
+    name: string;
+    password: string;
+    createdAt: Date;
+    updatedAt: Date;
+    comparePassword(candidatePassword: string): Promise<boolean>;
+}
+
+const UserSchema = new mongoose.Schema(
+    {
+        email: { type: String, required: true, unique: true },
+        name: { type: String, required: true },
+        password: { type: String, required: true },
+    },
+    { timestamps: true },
+);
+
+// Used for loggin In
+
+UserSchema.methods.comparePassword = async function(
+    candidatePassword: string
+) {
+    const user = this as UserDocument;
+
+    return bcrypt.compare(candidatePassword, user.password).catch((e) => false);
+}
+
+const User = mongoose.model<UserDocument>("User", UserSchema);
+
+export default User;
--- src/routes.ts
+++ src/routes.ts
@@ -2,4 +2,9 @@ import { Express, Request, Response } from 'express';
 
 export default function (app: Express) {
     app.get("/healthcheck", (req: Request, res: Response) => res.sendStatus(200));
+
+    // Register User
+    // POST/api/user
+
+    
 }
