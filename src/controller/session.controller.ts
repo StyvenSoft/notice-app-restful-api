cb29323a970011a99f8444d9a61ebe82d5750e4c
Steveen <steveen.echeverri@gmail.com>
2021-08-19 21:48:30 -0500

    createSession controller validate email and password

--- src/controller/session.controller.ts
+++ src/controller/session.controller.ts
@@ -1,4 +1,15 @@
-export async function createUserSessionHandler(req: Request, res: Request) {
+import { Request, Response } from "express";
+import { validatePassword } from "../service/user.service";
+import { createSession } from "../service/session.service";
+
+export async function createUserSessionHandler(req: Request, res: Response) {
     // Validate the email and password
-    
+    const user = await validatePassword(req.body);
+
+    if(!user) {
+        return res.status(401).send("Invalid Username or Password");
+    }
+
+    // Create a session
+    const session = await createSession(user_id, req.get("user-agent") || "");
 }
